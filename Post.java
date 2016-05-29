8ab3ee04c3cfcdbf725c670144fba1758369fd3b
tvtgela <gmagh13@freeuni.edu.g>
2016-05-29 15:08:20 +0400

    Create the classes we need

---
+++ Post.java
@@ -0,0 +1,19 @@
+package Core;
+
+public class Post {
+	
+	private int postId;
+	private String postTitle;
+	private String postText;
+	private int postUserId;
+	private int postType;
+	private String postTopic;
+	private int postCloud;
+	private int postUncloud;
+	
+	public Post(int postId, String postTitle, String postText, int postUserId, int postType,
+			String postTopic, int postCloud, int postUncloud){
+		
+	}
+	
+}
---
+++ User.java
@@ -0,0 +1,22 @@
+package Core;
+
+public class User {
+	
+	private int userId;
+	private String userName;
+	private String userSurname;
+	private String userNickName;
+	private String userEmail;
+	private String userPassword;
+	private int userAge;
+	private int userLevel;
+	private int userSex;
+	private String userMoto;
+	private String userImgSrc;	
+	
+	public User(int userId, String userName, String userNickName, String userEmail, String userPassword, 
+			int userAge, int userLevel, int userSex, String userMoto, String userImgSrc){
+		
+	}
+	
+}
