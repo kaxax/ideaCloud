1eb9e5807df5204c6a1f2fd0f0f9472985f80afa
tvtgela <gmagh13@freeuni.edu.g>
2016-05-29 16:15:05 +0400

    Add project with starter classes

---
+++ Project/src/Core/Comment.java
@@ -0,0 +1,19 @@
+package Core;
+
+public class Comment {
+	
+	private int CommentId;
+	private int CommentPostId;
+	private int CommentUserId;
+	private int CommentCloud;
+	private int CommentUncloud;
+	private String CommentText;
+	
+	public Comment(int CommentId, int CommentPostId , int CommentUserId, int CommentCloud,
+			int CommentUncloud, String CommentText){
+		 
+	}
+	
+	
+	
+}
