c3c3a6dcb803cb101366f75b124ee62559550f87
gooogi <gomia13@freeuni.edu.ge>
2016-06-12 19:18:24 +0400

    implemented connection pool

---
+++ Project/src/Core/Pool.java
@@ -0,0 +1,35 @@
+package Core;
+
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class Pool {
+	private static Pool connectionPool;
+	private BasicDataSource data;
+
+	private Pool() throws IOException, SQLException, PropertyVetoException {
+		data = new BasicDataSource();
+		data.setDriverClassName("com.mysql.jdbc.Driver");
+		data.setUsername("root");
+		data.setPassword("gela123");
+		data.setUrl("");
+	}
+
+	public static Pool getPool() throws IOException, SQLException,
+			PropertyVetoException {
+		if (connectionPool == null) {
+			connectionPool = new Pool();
+			return connectionPool;
+		} else {
+			return connectionPool;
+		}
+	}
+
+	public Connection getConnection() throws SQLException {
+		return this.data.getConnection();
+	}
+}
