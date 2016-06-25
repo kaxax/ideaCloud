2c22d9f34f1b490794796d3d7791efa0db4eb82a
BartholomewKuma27 <igugu13@freeuni.edu.ge>
2016-06-25 15:56:50 +0400

    handling login info

--- Project/src/Core/checkUser.java
+++ Project/src/Core/checkUser.java
@@ -9,8 +9,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import Core.Database;
 import Core.Pool;
@@ -41,15 +46,15 @@ public class checkUser extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Map<String, Object> map =  new HashMap<String, Object>();
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		try {
+		int id;
+		/*
+		 * try {
 			Connection con = Pool.getPool().getConnection();
 			Database db = new Database(con);
 			int id = db.getId(username, password);
-			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println(id);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -57,6 +62,34 @@ public class checkUser extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		 */
+		id = 15; 
+		if (id != -1){
+			saveSession(request);
+		}
+		map.put("id", id);
+		map.put("username", username);
+		map.put("password", password);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(new Gson().toJson(map));
+		
+	}
+
+	private void saveSession(HttpServletRequest request) {
+		HttpSession ses = request.getSession(false);
+		String username = null;
+		try {
+			username = (String) request.getParameter("username");
+		}
+		catch(Exception e){
+		}
+		if (username != null){
+			String password = (String) request.getParameter("test");
+			ses.setAttribute("username", username);
+			ses.setAttribute("password", password);
+		}
+		
 	}
 
 }
